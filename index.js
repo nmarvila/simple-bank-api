f9cfe3c89a7ea12a4f28a66c5ea6894a1714ff9b
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-08 19:12:45 -0300

    feat: create balance endpoint

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let contas = [
+    { "id": 1, "balance": 100 }
+];
 
 app.get('/', (req, res) => {
     res.send('Simple Bank API')
+})
+
+app.get('/balance', (req, res) => {
+    let conta = req.query.account_id
+
+    contas.find((c) => {
+        c.id == conta ? res.send(c.balance.toString()) : res.sendStatus(404);
+    })
 })
 
 app.listen(port, () => {
