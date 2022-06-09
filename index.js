cbe3f4a0d04fcace819bc84c09ab88c69f01ffc0
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-09 00:28:26 -0300

    feat: create event endpoint and deposit option

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-let contas = []
+const contas = []
 
 app.use(bodyParser.json())
 
@@ -20,6 +20,26 @@ app.get('/balance', (req, res) => {
     let conta = req.query.account_id
 
     contas.length > 0 ? contas.find((c) => c.id == conta ? res.send(c.balance.toString()) : res.sendStatus(404)) : res.sendStatus(404)
+})
+
+app.post('/event', (req, res) => {
+    let tipo = req.body.type
+    let destino = req.body.destination
+    let valor = req.body.amount
+
+    if (tipo == 'deposit') {
+        let conta = contas.find((c) => c.id == destino)
+
+        if (conta != undefined) {
+            conta.balance += valor
+            res.status(201).send({ 'destination': conta })
+        }
+        else {
+            let c = { 'id': destino, 'balance': valor }
+            contas.push(c)
+            res.status(201).send({ 'destination': c })
+        }
+    }
 })
 
 app.listen(port, () => {
