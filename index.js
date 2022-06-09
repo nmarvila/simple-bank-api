3ecee08e345831ab77295015e0e0a76ca58c0a39
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-08 23:25:59 -0300

    feat: create reset endpoint and refactor balance endpoint

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-let contas = [
-    { "id": 1, "balance": 100 }
-]
+let contas = [{ "id": 1, "balance": 100 }]
 
 app.use(bodyParser.json())
 
@@ -13,12 +11,15 @@ app.get('/', (req, res) => {
     res.send('Simple Bank API')
 })
 
+app.post('/reset', (req, res) => {
+    contas = []
+    res.sendStatus(200)
+})
+
 app.get('/balance', (req, res) => {
     let conta = req.query.account_id
 
-    contas.find((c) => {
-        c.id == conta ? res.send(c.balance.toString()) : res.sendStatus(404)
-    })
+    contas.length > 0 ? contas.find((c) => c.id == conta ? res.send(c.balance.toString()) : res.sendStatus(404)) : res.sendStatus(404)
 })
 
 app.listen(port, () => {
