536dec394a294e87a87c433c94290fc8c71fbf94
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-10 01:41:42 -0300

    refactor: transfer event logic to its specific route file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const contas = require('./repository/contas')
 
 const resetRoute = require('./routes/reset')
 const balanceRoute = require('./routes/balance')
+const eventRoute = require('./routes/event')
 
 const app = express()
 const port = 3000
@@ -16,29 +16,7 @@ app.get('/', (req, res) => {
 
 app.use('/reset', resetRoute)
 app.use('/balance', balanceRoute)
-
-app.post('/event', (req, res) => {
-    switch (req.body.type) {
-        case 'deposit':
-            res.status(201).send({ 'destination': contas.find(c => c.id == req.body.destination ? c.balance += req.body.amount : undefined) ?? contas[contas.push({ 'id': req.body.destination, 'balance': req.body.amount }) - 1] })
-            break;
-
-        case 'withdraw':
-            res.status(201).send({ 'origin': contas.find(c => c.id == req.body.origin ? c.balance -= req.body.amount : undefined) ?? res.status(404).send('0') })
-            break;
-
-        case 'transfer':
-            let contaOrigem = contas.find(c => c.id == req.body.origin) ?? res.status(404).send('0')
-            let contaDestino = contas.find(c => c.id == req.body.destination) ?? contas[contas.push({ 'id': req.body.destination, 'balance': 0 }) - 1]
-            contaOrigem.balance -= req.body.amount
-            contaDestino.balance += req.body.amount
-            res.status(201).send({ 'origin': contaOrigem, 'destination': contaDestino })
-            break;
-
-        default:
-            break;
-    }
-})
+app.use('/event', eventRoute)
 
 app.listen(port, () => {
     console.log(`Simple Bank API listening on port ${port}`)
