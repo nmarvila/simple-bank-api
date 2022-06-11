584e0180afe708511dc22357ad12f7ef1ab4c0f8
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-11 17:00:05 -0300

    refactor: move balance http status handling from service to controller

--- controllers/balance.js
+++ controllers/balance.js
@@ -1,5 +1,11 @@
 const balanceService = require('../services/balance')
 
 module.exports.getBalance = (req, res) => {
-    balanceService.getBalance(req, res)
+    let balance = balanceService.getBalance(req, res)
+
+    if (balance == undefined) {
+        res.status(404).send('0')
+    } else {
+        res.status(200).send(balance)
+    }
 }
--- services/balance.js
+++ services/balance.js
@@ -4,9 +4,5 @@ module.exports.getBalance = (req, res) => {
     let account_id = req.query.account_id
 
     let balance = accounts.getBalance(account_id)
-    if (balance == undefined) {
-        res.status(404).send('0')
-    } else {
-        res.status(200).send(balance)
-    }
+    return balance
 }
