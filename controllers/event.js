f11d524f5159a182c27f16b733dcffd63660ed02
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-11 17:12:28 -0300

    refactor: move event http status handling from service to controller

--- controllers/event.js
+++ controllers/event.js
@@ -1,5 +1,32 @@
 const eventService = require('../services/event')
 
 module.exports.callEvent = (req, res) => {
-    eventService.callEvent(req, res)
+    let type = req.body.type
+
+    let resultAccounts = eventService.callEvent(req, res)
+
+    switch (type) {
+        case 'deposit':
+            res.status(201).send({ 'destination': resultAccounts })
+            break;
+
+        case 'withdraw':
+            if (resultAccounts == undefined) {
+                res.status(404).send('0')
+            } else {
+                res.status(201).send({ 'origin': resultAccounts })
+            }
+            break;
+
+        case 'transfer':
+            if (resultAccounts.originAccount == undefined) {
+                res.status(404).send('0')
+            } else {
+                res.status(201).send({ 'origin': resultAccounts.originAccount, 'destination': resultAccounts.destinationAccount })
+            }
+            break;
+
+        default:
+            break;
+    }
 }
--- services/event.js
+++ services/event.js
@@ -9,26 +9,15 @@ module.exports.callEvent = (req, res) => {
     switch (type) {
         case 'deposit':
             let depositAccount = accounts.deposit(destination, amount)
-            res.status(201).send({ 'destination': depositAccount })
-            break;
+            return depositAccount
 
         case 'withdraw':
             let withdrawAccount = accounts.withdraw(origin, amount)
-            if (withdrawAccount == undefined) {
-                res.status(404).send('0')
-            } else {
-                res.status(201).send({ 'origin': withdrawAccount })
-            }
-            break;
+            return withdrawAccount
 
         case 'transfer':
             let resultAccounts = accounts.transfer(origin, destination, amount)
-            if (resultAccounts.originAccount == undefined) {
-                res.status(404).send('0')
-            } else {
-                res.status(201).send({ 'origin': resultAccounts.originAccount, 'destination': resultAccounts.destinationAccount })
-            }
-            break;
+            return resultAccounts
 
         default:
             break;
