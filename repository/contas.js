8462c65c6462cd0f9782eb581dd9974cc7e88f24
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-11 00:04:01 -0300

    refactor: add getBalance to the exported methods of the repository

--- repository/contas.js
+++ repository/contas.js
@@ -3,3 +3,12 @@ let contas = []
 module.exports.resetState = () => {
     contas = []
 }
+
+module.exports.getBalance = (account_id) => {
+    let contaConsultada = contas.find(conta => conta.id == account_id)
+    if (contaConsultada == undefined) {
+        return undefined
+    } else {
+        return contaConsultada.balance.toString()
+    }
+}
--- services/balance.js
+++ services/balance.js
@@ -1,10 +1,10 @@
 const contas = require('../repository/contas')
 
 module.exports.getBalance = (req, res) => {
-    let conta = contas.contas.find(c => c.id == req.query.account_id)
-    if (conta == undefined) {
+    let saldo = contas.getBalance(req.query.account_id)
+    if (saldo == undefined) {
         res.status(404).send('0')
     } else {
-        res.status(200).send(conta.balance.toString())
+        res.status(200).send(saldo)
     }
 }
