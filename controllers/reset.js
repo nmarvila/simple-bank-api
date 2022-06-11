48cb07d928cd1da501877b2b4e0c59cb8e5c53ee
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-11 17:02:17 -0300

    refactor: move reset http status handling from service to controller

--- controllers/reset.js
+++ controllers/reset.js
@@ -2,4 +2,5 @@ const resetService = require('../services/reset')
 
 module.exports.resetState = (req, res) => {
     resetService.resetState(req, res)
+    res.sendStatus(200)
 }
--- services/reset.js
+++ services/reset.js
@@ -2,5 +2,4 @@ const accounts = require('../repository/accounts')
 
 module.exports.resetState = (req, res) => {
     accounts.resetState()
-    res.sendStatus(200)
 }
