b7799fa0efc64c95e5eefb5af313e72cdc473cf7
Nichollas Marvila <nichollasmarvila@gmail.com>
2022-06-10 22:59:49 -0300

    refactor: move balance service logic from the controller file to the service file

--- controllers/balance.js
+++ controllers/balance.js
@@ -1,10 +1,5 @@
 const balanceService = require('../services/balance')
 
 module.exports.getBalance = (req, res) => {
-    const balanceValue = balanceService.getBalance(req, res)
-    if (balanceValue == undefined) {
-        res.status(404).send('0')
-    } else {
-        res.status(200).send(balanceValue.toString())
-    }
+    balanceService.getBalance(req, res)
 }
--- services/balance.js
+++ services/balance.js
@@ -3,8 +3,8 @@ const contas = require('../repository/contas')
 module.exports.getBalance = (req, res) => {
     let conta = contas.contas.find(c => c.id == req.query.account_id)
     if (conta == undefined) {
-        return undefined
+        res.status(404).send('0')
     } else {
-        return conta.balance
+        res.status(200).send(conta.balance.toString())
     }
 }
